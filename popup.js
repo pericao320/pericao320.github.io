dd5c4b95cd32885fdbd32226b678937ad61b2522
pericao320 <101295457+pericao320@users.noreply.github.com>
2022-03-17 01:09:50 -0500

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-var btnAbrirpopup = document.getElementById('avance-edtd'),
+let btnAbrirpopup = document.getElementById('avance-edtd'),
     overlay = document.getElementById('overlay'),
     popup = document.getElementById('popup-edtd'),
     btnCerrarpopup = document.getElementById('btn-cerrar-popup');
@@ -9,6 +9,11 @@ btnAbrirpopup.addEventListener('click', function () {
 });
 
 btnCerrarpopup.addEventListener('click', function () {
+    overlay.classList.remove('active');
+    popup.classList.remove('active');
+});
+
+overlay.addEventListener('click', function () {
     overlay.classList.remove('active');
     popup.classList.remove('active');
 });

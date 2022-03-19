d90274fa19d2fcd39506469f9b0a38f03a47e8cb
pericao320 <101295457+pericao320@users.noreply.github.com>
2022-03-18 21:01:08 -0500

    Initial commits

--- popup.js
+++ popup.js
@@ -1,7 +1,7 @@
-let btnAbrirpopup = document.getElementById('avance-edtd'),
-    overlay = document.getElementById('overlay'),
-    popup = document.getElementById('popup-edtd'),
-    btnCerrarpopup = document.getElementById('btn-cerrar-popup');
+const btnAbrirpopup = document.getElementById('avance-edtd'),
+      overlay = document.getElementById('overlay'),
+      popup = document.getElementById('popup-edtd'),
+      btnCerrarpopup = document.getElementById('btn-cerrar-popup');
 
 btnAbrirpopup.addEventListener('click', function () {
     overlay.classList.add('active');
@@ -17,3 +17,23 @@ overlay.addEventListener('click', function () {
     overlay.classList.remove('active');
     popup.classList.remove('active');
 });
+
+/*- OVERLAY DE DISPONIVILIDAD -*/
+
+const btnAbrirPopUpDisp = document.getElementById('reproducir-edtd'),
+    btnCerrarPopUpDisp = document.getElementById('cerrar-overlay-disp'),
+    popUpDisp = document.getElementById('overlay-disp'),
+    contentpopUpDisp = document.getElementById('contenedor-overlay-de-disponibilidad');
+
+let cerrarPopUpDisp = function () {
+    contentpopUpDisp.classList.remove('active');
+    popUpDisp.classList.remove('active');
+}
+
+btnAbrirPopUpDisp.addEventListener('click', function () {
+    contentpopUpDisp.classList.add('active');
+    popUpDisp.classList.add('active');
+    setTimeout(cerrarPopUpDisp, 3500);
+});
+
+btnCerrarPopUpDisp.addEventListener('click', cerrarPopUpDisp);
